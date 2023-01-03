6199a90e6c421fc510611c432f9f3430bdf686c0
Michał Boczula <Michal.Boczula@kruksa.pl>
2023-01-03 12:55:05 +0100

    added validation

--- src/app/BasicForm/basic-form.component.ts
+++ src/app/BasicForm/basic-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { BasicFromModule } from './basic-from-model';
 
 @Component({
@@ -8,14 +9,24 @@ import { BasicFromModule } from './basic-from-model';
 })
 export class BasicFormComponent {
   private basicFromModule: BasicFromModule = {
-    name: "test",
+    name: '',
     email: true,
     color: "black",
     option: "first",
     info: "lorem ipsum"
   }
 
+  show:boolean = true;
   basicFromModuleVM: BasicFromModule = { ...this.basicFromModule }
 
+  onClick(){
+    this.show = !this.show;
+    setTimeout(() => this.show= !this.show, 2000);
+  }
+
+  onSubmit(form: NgForm)
+  {
+    console.log(form);
+  }
 
 }
